e500457220e2820bc37aaadd19d3addcddb87b67
donerducktales <randomercook231@gmail.com>
2023-09-07 18:08:21 +0300

    Has been configured react-roater-dom and added some pages like Home and Converter. Also has been added Header and Layout components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Converter from "./pages/Converter";
+
 export default function App() {
    return (
       <div className="App">
-      
+         <Router>
+            <Routes>
+               <Route path="/" element={<Layout />}>
+                  <Route element={<Home />} index='true' />
+                  <Route path="/converter" element={<Converter />} />
+               </Route>
+            </Routes>
+         </Router>
       </div>
    );
 }
