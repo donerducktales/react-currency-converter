66e2c4af1855bd2402f660a9d1e0aaa4115addda
donerducktales <randomercook231@gmail.com>
2023-09-07 19:15:29 +0300

    Has been configured nav links in Header component, installed react roater bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+   return (
+      <>
+         <Navbar bg='dark' data-bs-theme='dark'>
+            <Container fluid>
+               <Navbar.Brand>
+                  Just Converter
+               </Navbar.Brand>
+               <Nav className="me-auto">
+                  <LinkContainer to='/'>
+                     <Nav.Link>
+                        Home
+                     </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/converter'>
+                     <Nav.Link>
+                        Converter
+                     </Nav.Link>
+                  </LinkContainer>
+               </Nav>
+            </Container>
+         </Navbar>
+         
+      </>
+   )
 }
